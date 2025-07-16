1f94b4da77c99c3a3149225a7fb1447fde21b9a3
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2025-07-16 11:29:53 +0300

    Create entities classes and repositories for them

---
+++ accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -0,0 +1,24 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Accounts extends BaseEntity{
+    private Long customerId;
+
+    @Id
+    private String accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
