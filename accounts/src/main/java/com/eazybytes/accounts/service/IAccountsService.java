aea74bf44345fdc39935a5b50264101e4695b07d
rostyslav-berezhnytskyi <rostik2510@gmail.com>
2025-07-20 23:30:38 +0300

    Add validation to dto, expend globalExceptionHandler functionality, add methods for update and delete to AccountService

--- accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/IAccountsService.java
@@ -16,4 +16,18 @@ public interface IAccountsService {
      * @return Accounts Details based on a given mobileNumber
      */
     CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     *
+     * @param  customerDto - Input CustomerDto object
+     * @return boolean indicating if the update of Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - input mobile number
+     * @return boolean indicating if the delete of Account details is successful
+     */
+    boolean deleteAccount(String mobileNumber);
 }
